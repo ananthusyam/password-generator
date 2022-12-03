7747369e39b1e1924a71cb60e487f9aeea45e002
ananthusyam <109689808+ananthusyam@users.noreply.github.com>
2022-12-03 14:20:08 +0530

    changes made

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 let isAlive = true;
-let live = true;
+
 
 
 let passOneEl = document.getElementById("pass-one")
@@ -15,10 +15,7 @@ function password() {
             passOneEl.textContent = passone.join('')
            
         }
-    }
-
-    if(live === true){
-        for(let i = 0; i < 8; i++) {
+        for(let j = 0; j < 8; j++) {
             
             passtwo.push(characters[Math.floor(Math.random()*characters.length)]);
             passTwoEl.textContent = passtwo.join('')
